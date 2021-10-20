fc4b4c72fd7561fed7a86312c9a7794499825fb8
Veaceslav Chirilov <veaceslav.chirilov@endava.com>
2021-10-20 17:52:30 +0300

    First completed application using EFCore

---
+++ EntityFrameworkCore/PR01_HelloApp/ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PR01_HelloApp
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=myDataBase;Integrated Security=SSPI;");
+        }
+    }
+}
--- EntityFrameworkCore/PR01_HelloApp/Program.cs
+++ EntityFrameworkCore/PR01_HelloApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace PR01_HelloApp
 {
@@ -6,6 +7,28 @@ namespace PR01_HelloApp
     {
         static void Main(string[] args)
         {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                // создаем два объекта User
+                User user1 = new User { Name = "Tom", Age = 33 };
+                User user2 = new User { Name = "Alice", Age = 26 };
+
+                // добавляем их в бд
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+                Console.WriteLine("Objects were saved");
+
+                // получаем объекты из бд и выводим на консоль
+                var users = db.Users.ToList();
+                Console.WriteLine("List of objects were created");
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+            
+            Console.Read();
             Console.WriteLine("Hello World!");
         }
     }
